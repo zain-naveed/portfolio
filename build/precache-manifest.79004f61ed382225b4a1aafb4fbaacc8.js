6fdf27ba24f6e589e48820bfdfa37a7e02c3b980
zainnaveed <zain.naveed@ropstam.com>
2024-02-09 16:48:30 +0500

    Build deploy

---
+++ build/precache-manifest.79004f61ed382225b4a1aafb4fbaacc8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea64a27083bf75d1d5bbb2a9a1f731e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b58032fd2433c434a72c",
+    "url": "/portfolio/static/css/main.f13ddba0.chunk.css"
+  },
+  {
+    "revision": "4728e2f97beb6d4e0c67",
+    "url": "/portfolio/static/js/2.926906a5.chunk.js"
+  },
+  {
+    "revision": "b58032fd2433c434a72c",
+    "url": "/portfolio/static/js/main.e57fb831.chunk.js"
+  },
+  {
+    "revision": "55ffacab19f08418ce55",
+    "url": "/portfolio/static/js/runtime-main.99bbf566.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
